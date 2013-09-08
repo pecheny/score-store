9b1fcfc62f309f2d31d5f0783df52484830e4757
artem pecheny <pecheny@gmail.com>
2013-09-09 02:09:58 +0600

    PlayerView: balloon + textfield

--- Source/Main.hx
+++ Source/Main.hx
@@ -1,6 +1,7 @@
 package;
 
 
+import view.PlayerView;
 import signals.RemovePlayerSignal;
 import flash.text.TextField;
 import flash.geom.Matrix;
@@ -18,39 +19,42 @@ class Main extends Sprite {
     public function new() {
 
         super();
-        var swf:SWF = new SWF (Assets.getBytes("assets/CounterAssets.swf"));
-        var testButton:Sprite = swf.createMovieClip("PlayerUnitView");
-        addChild(testButton);
-testButton.transform.matrix = new Matrix();
-                var name:TextField = cast testButton.getChildByName("_name");
-                initTextField(name);
-        var sig = new RemovePlayerSignal(Int);
-    //        name.text = "Name";
-    //        name.textColor = 0xffffff;
-    //        name.defaultTextFormat = mTFormat;
-    //        name.embedFonts = true;
-
-           //     trace(name);
+        var pView = new PlayerView();
+        addChild(pView);
+        pView.setText("ololo");
+//        var swf:SWF = new SWF (Assets.getBytes("assets/CounterAssets.swf"));
+//        var testButton:Sprite = swf.createMovieClip("PlayerUnitView");
+//        addChild(testButton);
+//testButton.transform.matrix = new Matrix();
+//                var name:TextField = cast testButton.getChildByName("_name");
+//                initTextField(name);
+//        var sig = new RemovePlayerSignal(Int);
+//    //        name.text = "Name";
+//    //        name.textColor = 0xffffff;
+//    //        name.defaultTextFormat = mTFormat;
+//    //        name.embedFonts = true;
+//
+//           //     trace(name);
 
     }
 
 
-public function initTextField(sourceTextField:TextField){
-        var fontAsset:Font = Assets.getFont("assets/DroidSans-Bold.ttf");
-        var mTFormat:TextFormat = new TextFormat(fontAsset.fontName, sourceTextField.textHeight);
-        mTFormat.color =0xffffff;
-        var parent:DisplayObjectContainer = cast (sourceTextField.parent);
-
-
-        var mTF:TextField = new TextField();
-        mTF.defaultTextFormat = mTFormat;
-        mTF.embedFonts = true;
-        mTF.height = sourceTextField.height;
-        mTF.width = sourceTextField.width;
-        mTF.text = sourceTextField.text;
-        mTF.transform.matrix = sourceTextField.transform.matrix;
-        parent.removeChild(sourceTextField);
-        parent.addChild(mTF);
-}
+//public function initTextField(sourceTextField:TextField){
+//        var fontAsset:Font = Assets.getFont("assets/DroidSans-Bold.ttf");
+//        var mTFormat:TextFormat = new TextFormat(fontAsset.fontName, sourceTextField.textHeight);
+//        mTFormat.color =0xffffff;
+//        var parent:DisplayObjectContainer = cast (sourceTextField.parent);
+//
+//
+//        var mTF:TextField = new TextField();
+//        mTF.defaultTextFormat = mTFormat;
+//        mTF.embedFonts = true;
+//        mTF.height = sourceTextField.height;
+//        mTF.width = sourceTextField.width;
+//        mTF.text = sourceTextField.text;
+//        mTF.transform.matrix = sourceTextField.transform.matrix;
+//        parent.removeChild(sourceTextField);
+//        parent.addChild(mTF);
+//}
 
 }
--- Source/view/PlayerView.hx
+++ Source/view/PlayerView.hx
@@ -1,5 +1,38 @@
 package view;
+import flash.text.TextFormat;
+import flash.text.TextField;
+import openfl.Assets;
+import flash.text.Font;
 import flash.display.Sprite;
-class PlayerView extends Sprite{
+class PlayerView extends Sprite {
+    private var mTF:TextField;
 
+    public function new() {
+        super();
+        graphics.beginFill(0, 0.75);
+        graphics.drawRoundRect(0, 0, 300, 100, 20, 20);
+        graphics.endFill();
+        initTextField();
+    }
+
+    private function initTextField() {
+        var fontAsset:Font = Assets.getFont("assets/DroidSans-Bold.ttf");
+        var mTFormat:TextFormat = new TextFormat(fontAsset.fontName, 30);
+        mTFormat.color = 0xffffff;
+        mTF = new TextField();
+        mTF.defaultTextFormat = mTFormat;
+        mTF.embedFonts = true;
+        mTF.x = 30;
+        mTF.y = 30;
+//            mTF.height = sourceTextField.height;
+//            mTF.width = sourceTextField.width;
+//            mTF.text = sourceTextField.text;
+//            mTF.transform.matrix = sourceTextField.transform.matrix;
+//            parent.removeChild(sourceTextField);
+        addChild(mTF);
+    }
+
+    public function setText(text:String):Void {
+        mTF.text = text;
+    }
 }
