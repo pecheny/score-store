f79f3c0eb8189b4bbaaafda94db477e227ca326c
artem pecheny <pecheny@gmail.com>
2013-10-03 14:36:10 +0600

    Update layout after player remove

--- Source/commands/RemovePlayerCommand.hx
+++ Source/commands/RemovePlayerCommand.hx
@@ -1,4 +1,5 @@
 package commands;
+import signals.UpdateLayoutSignal;
 import signals.RemoveChildSignal;
 import view.PlayerView;
 import view.ApplicationView;
@@ -13,12 +14,14 @@ class RemovePlayerCommand extends mmvc.impl.Command {
     @inject public var appView:ApplicationView;
     @inject public var playerId:PlayerId;
     @inject public var removeChildSignal:RemoveChildSignal;
+    @inject public var updateLayoutSignal:UpdateLayoutSignal;
 
     override public function execute():Void {
         playersModel.disablePlayer(playerId);
         var pview:PlayerView = playerViewsModel.getView(playerId);
         playerViewsModel.removeView(playerId);
         removeChildSignal.dispatch(pview);
+        updateLayoutSignal.dispatch();
     }
 
 }
