265e7a95f9f5b1389d78970d5c4f8ff1d2f1ed76
artem pecheny <pecheny@gmail.com>
2013-10-05 16:45:33 +0600

    ButtonBar vertical align

--- Source/commands/LayoutViewsCommand.hx
+++ Source/commands/LayoutViewsCommand.hx
@@ -1,15 +1,15 @@
 package commands;
+import view.ApplicationView;
 import model.ButtonBarContainerModel;
 import view.ViewBase;
 import constants.PlayerViewStyle;
 import model.PlayerModel;
 import model.PlayerViewsModel;
 class LayoutViewsCommand extends mmvc.impl.Command {
-    @inject
-    public var playersModel:PlayerModel;
-    @inject
-    public var playerViewsModel:PlayerViewsModel;
+    @inject public var playersModel:PlayerModel;
+    @inject public var playerViewsModel:PlayerViewsModel;
     @inject public var buttonBarContainerModel:ButtonBarContainerModel;
+    @inject public var applicationView:ApplicationView;
 
     override public function execute():Void {
         var players = playersModel.getPlayers().iterator();
@@ -20,7 +20,16 @@ class LayoutViewsCommand extends mmvc.impl.Command {
             y += viewInstance.getHeight() + PlayerViewStyle.GRID_STEP;
         }
 
-        buttonBarContainerModel.butonBarContainer.y = y;
+        buttonBarContainerModel.butonBarContainer.y = isFitInScreen()?
+            applicationView.getStageHeight() / applicationView.calculateScale() - PlayerViewStyle.UNIT_HEIGHT - PlayerViewStyle.GRID_STEP
+            :y;
+    }
+
+    private function isFitInScreen():Bool {
+        var numUnits = playersModel.getPlayers().length + 1;
+           var unitsHeight = numUnits * (PlayerViewStyle.UNIT_HEIGHT + PlayerViewStyle.GRID_STEP) + PlayerViewStyle.GRID_STEP;
+           var stageHeight = applicationView.getStageHeight() / applicationView.calculateScale();
+           return unitsHeight < stageHeight;
     }
 
 }
