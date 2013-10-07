013ff0c2e25700750f1ad97266d23825cb8bdd05
artem pecheny <pecheny@gmail.com>
2013-10-08 01:46:23 +0600

    Disable exit action for flash target

--- Source/commands/EmptyCommand.hx
+++
@@ -1,8 +0,0 @@
-package commands;
-class EmptyCommand extends mmvc.impl.Command {
-
-    override public function execute():Void {
-        trace(signal);
-    }
-
-}
--- Source/commands/ExitCommand.hx
+++ Source/commands/ExitCommand.hx
@@ -3,7 +3,9 @@ import flash.system.System;
 class ExitCommand extends mmvc.impl.Command {
 
     override public function execute():Void {
+    #if mobile
         System.exit(0);
+    #end
     }
 
 }
