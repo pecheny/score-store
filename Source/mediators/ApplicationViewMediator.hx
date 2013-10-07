664547b3dfdef00af1d416f2335802b6c0ce1637
artem pecheny <pecheny@gmail.com>
2013-10-07 15:15:26 +0600

    Handle back button

--- Source/mediators/ApplicationViewMediator.hx
+++ Source/mediators/ApplicationViewMediator.hx
@@ -1,4 +1,7 @@
 package mediators;
+import signals.CloseModalWindowSignal;
+import signals.ShowModalWindowSignal;
+import flash.events.KeyboardEvent;
 import flash.display.DisplayObject;
 import signals.RemoveChildSignal;
 import signals.AddChildSignal;
@@ -11,14 +14,21 @@ class ApplicationViewMediator extends mmvc.impl.Mediator<ApplicationView> {
     @inject public var resizeSignal:StageResizedSignal;
     @inject public var addChildSignal:AddChildSignal;
     @inject public var removeChildSignal:RemoveChildSignal;
+    @inject public var showModalWindowSignal:ShowModalWindowSignal;
+    @inject public var closeModalWindowSignal:CloseModalWindowSignal;
     var applicationView:ApplicationView;
+    var modalWindow:Bool;
 
 
     override public function onRegister() {
         applicationView = cast contextView;
         Lib.current.stage.addEventListener(Event.RESIZE, stageResizedHandler);
+        Lib.current.stage.addEventListener(KeyboardEvent.KEY_UP, keyboardUpHandler);
         addChildSignal.add(addChildHandler);
         removeChildSignal.add(removeChildHandler);
+
+        showModalWindowSignal.add(function(w:DisplayObject) {modalWindow = true;});
+        closeModalWindowSignal.add(function() {modalWindow = false;});
     }
 
     override public function preRemove():Void {
@@ -39,5 +49,14 @@ class ApplicationViewMediator extends mmvc.impl.Mediator<ApplicationView> {
 
     private function removeChildHandler(child:DisplayObject):Void {
         applicationView.removeChild(child);
+    }
+
+    private function keyboardUpHandler(e:KeyboardEvent):Void {
+        if (e.keyCode == 27) {
+            e.stopImmediatePropagation();
+            if(modalWindow) {
+            closeModalWindowSignal.dispatch();
+            }
+        }
     }
 }
